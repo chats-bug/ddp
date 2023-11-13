256e90ffbaabdaea4b34ecb6f07877e7c3bbdeae
Sukrit <chatterjeesukrit@outlook.com>
2023-11-13 16:14:22 +0530

    exposed `get_ddp_dataloader`

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .data import get_dataset, CustomDataset, PoorMansDataLoader
+from .data import get_dataset, CustomDataset, PoorMansDataLoader, get_ddp_dataloader
 from .model import get_llama_model
-from .utils import format_float_to_str, num_trainable_params
 from .optim import WarmupCosineWithDecay
+from .utils import format_float_to_str, num_trainable_params
