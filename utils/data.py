11ed0c8da18158301b7aee91f6fcab73c1bfa803
Sukrit <chatterjeesukrit@outlook.com>
2023-11-27 16:20:51 +0530

    rewrite using context pool

--- utils/data.py
+++ utils/data.py
@@ -1,17 +1,14 @@
 from typing import Optional, Union, List
-
 import torch
 from datasets import load_dataset, Dataset
-from torch.utils.data import DataLoader, DistributedSampler
-from trl.trainer.utils import ConstantLengthDataset
 from transformers import PreTrainedTokenizerBase
 import multiprocessing as mp
+from tqdm import tqdm
+from multiprocessing import Pool, RLock
 if __name__ == "__main__":
     from concat_dataset import ConcatTokensDataset
 else:
     from .concat_dataset import ConcatTokensDataset
-from tqdm import tqdm
-from multiprocessing.pool import ThreadPool as Pool
 
 
 def get_dataset(
@@ -26,34 +23,15 @@ def get_dataset(
     return load_dataset(data_path, split=split, **kwargs)
 
 
-# This function will be run in parallel processes to process dataset partitions
-def process_partition(args):
-    (
-        hf_dataset_partition,
-        dataset_text_field,
-        tokenizer,
-        max_length,
-        bos_text,
-        eos_text,
-        no_wrap,
-        num_proc,
-        is_tokenized,
-    ) = args
-    return process_dataset(
-        hf_dataset_partition,
-        dataset_text_field,
-        tokenizer,
-        max_length,
-        bos_text,
-        eos_text,
-        no_wrap,
-        num_proc,
-        is_tokenized,
-    )
+def iterable_to_list(iterable):
+    samples = []
+    for sample in tqdm(iterable):
+        samples.append(sample)
+    return samples
 
 
 def process_dataset(
-    hf_dataset: Dataset,
+    dataset: Dataset,
     dataset_text_field: str,
     tokenizer: PreTrainedTokenizerBase,
     max_length: int,
@@ -64,7 +42,7 @@ def process_dataset(
     is_tokenized: bool = True,
 ):
     concat_tokens_dataset = ConcatTokensDataset(
-        hf_dataset=hf_dataset,
+        hf_dataset=dataset,
         dataset_text_field=dataset_text_field,
         tokenizer=tokenizer,
         max_length=max_length,
@@ -72,20 +50,17 @@ def process_dataset(
         eos_text=eos_text,
         no_wrap=no_wrap,
         num_proc=num_proc,
-        is_tokenized=is_tokenized,
         pre_tokenize=True,
+        is_tokenized=is_tokenized,
     )
+    samples = iterable_to_list(concat_tokens_dataset)
+    samples_tensor = torch.stack([sample["tokens"] for sample in samples], dim=0)
+    samples_tensor_dict = {"tokens": samples_tensor}
+    return samples_tensor_dict
 
-    packed_dataset_dict = {"tokens": torch.tensor([], dtype=torch.long)}
-    for sample in concat_tokens_dataset:
-        sample["tokens"] = sample["tokens"].unsqueeze(0)
-        if len(packed_dataset_dict["tokens"]) == 0:
-            packed_dataset_dict["tokens"] = sample["tokens"]
-            continue
-        packed_dataset_dict["tokens"] = torch.cat(
-            (packed_dataset_dict["tokens"], sample["tokens"]), dim=0
-        )
-    return packed_dataset_dict
+
+def process_dataset_wrapper(args):
+    return process_dataset(*args)
 
 
 def prepare_dataset(
@@ -98,15 +73,28 @@ def prepare_dataset(
     no_wrap: bool = False,
     num_proc: int | None = None,
     num_partitions: int | None = 4,
+    truncate: int = None,
+    disable_tqdm: bool = False,
 ) -> torch.Tensor:
     num_proc = mp.cpu_count() - 1 if num_proc is None else num_proc
     num_partitions = num_proc if num_partitions is None else num_partitions
-
+    chars_per_token = 5
+    if truncate is not None:
+        truncate = int(truncate)
+        hf_dataset = hf_dataset.map(
+            lambda x: {
+                dataset_text_field: x[dataset_text_field][: truncate * chars_per_token]
+            },
+            num_proc=num_proc,
+            # batched=True,
+            # batch_size=1,
+        )
     hf_dataset = hf_dataset.map(
         lambda x: tokenizer(
             x[dataset_text_field],
+            max_length=truncate,
             padding=False,
-            truncation=False,
+            truncation=bool(truncate),
             return_tensors="pt",
         ),
         num_proc=num_proc,
@@ -119,63 +107,36 @@ def prepare_dataset(
         f"Done tokenizing the dataset. Splitting into {num_partitions} partitions of size {sz}. \
             Total size: {len(hf_dataset)}::{sz*num_partitions}"
     )
-    # Split the tokenized dataset into num_partitions
-    partitions = [
-        hf_dataset.shard(num_partitions, i, contiguous=True)
-        for i in range(num_partitions)
-    ]
 
-    # Prepare arguments for parallel execution
-    args_list = [
-        (
-            partitions[i],
-            "text",
-            tokenizer,
-            max_length,
-            bos_text,
-            eos_text,
-            no_wrap,
-            num_proc,
-            True,
+    split_kwds = []
+    for index in range(num_partitions):
+        shard = hf_dataset.shard(num_partitions, index)
+        split_kwds.append(
+            (
+                shard,
+                dataset_text_field,
+                tokenizer,
+                max_length,
+                bos_text,
+                eos_text,
+                no_wrap,
+                num_proc,
+                True,
+            )
         )
-        for i in range(num_partitions)
-    ]
-    """
-    hf_dataset_partition,
-    tokenizer,
-    max_length,
-    bos_text,
-    eos_text,
-    no_wrap,
-    num_proc,
-    is_tokenized,
-    """
 
-    print("Creating a pool of worker processes")
-    # Create a pool of worker processes
-    pool = Pool(processes=num_partitions)
+    print(
+        f"Spawning {num_proc} processes for {len(hf_dataset)} objects in slices of {[len(i[0]) for i in split_kwds]}"
+    )
+    initargs, initializer = None, None
+    if not disable_tqdm:
+        initargs, initializer = (RLock(),), tqdm.set_lock
+    with Pool(num_partitions, initargs=initargs, initializer=initializer) as pool:
+        mapped = pool.map(process_dataset_wrapper, split_kwds)
+    print(f"Finished {num_partitions} processes.")
 
-    print("Processing dataset partitions in parallel")
-    # Process dataset partitions in parallel
-    results = pool.map(process_partition, args_list)
-
-    # Terminate the pool of workers
-    pool.close()
-    pool.join()
-
-    print("Merging the results")
-    # Merge the results into a single dataset
-    merged_dataset_dict = {"tokens": torch.tensor([], dtype=torch.long)}
-    for result in tqdm(results):
-        if len(merged_dataset_dict["tokens"]) == 0:
-            merged_dataset_dict["tokens"] = result["tokens"]
-            continue
-
-        merged_dataset_dict["tokens"] = torch.cat(
-            (merged_dataset_dict["tokens"], result["tokens"]), dim=0
-        )
-    return merged_dataset_dict["tokens"]
-
+    mapped_tokens = torch.cat([i["tokens"] for i in mapped], dim=0)
+    return mapped_tokens
 
 
 if __name__ == "__main__":
@@ -189,15 +150,19 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_name", type=str, default="roneneldan/TinyStories")
     parser.add_argument("--subset", type=float, default=0.0)
     parser.add_argument("--dataset_text_field", type=str, default="text")
-    parser.add_argument("--tokenizer_name", type=str, default="meta-llama/Llama-2-7b-hf")
+    parser.add_argument(
+        "--tokenizer_name", type=str, default="meta-llama/Llama-2-7b-hf"
+    )
     parser.add_argument("--max_length", type=int, default=512)
     parser.add_argument("--bsz", type=int, default=4)
-    parser.add_argument("--num_proc", type=int, default=4)
-    parser.add_argument("--num_partitions", type=int, default=4)
+    parser.add_argument("--num_proc", type=int, default=8)
+    parser.add_argument("--num_partitions", type=int, default=8)
 
     args = parser.parse_args()
 
-    print(f"Loading dataset: {args.dataset_name}, subset: {args.subset if args.subset > 0.0 else 'all'}")
+    print(
+        f"Loading dataset: {args.dataset_name}, subset: {args.subset if args.subset > 0.0 else 'all'}"
+    )
     dataset = load_dataset(args.dataset_name, split="train")
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
     tokenizer.pad_token = tokenizer.eos_token
@@ -213,13 +178,12 @@ if __name__ == "__main__":
         dataset_text_field=args.dataset_text_field,
         tokenizer=tokenizer,
         max_length=args.max_length,
-        bos_text=tokenizer.bos_token,
-        eos_text=tokenizer.eos_token,
+        bos_text="",
+        eos_text="",
         num_proc=args.num_proc,
         num_partitions=args.num_partitions,
     )
 
-    
     dataloader = DataLoader(
         prepared_dataset,
         batch_size=args.bsz,
