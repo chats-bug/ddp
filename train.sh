daeb5643a84efe33843ad2a50db659866523eb62
chats-bug <chatterjeesukrit@outook.com>
2023-12-08 13:58:17 +0000

    using torchrun to do fsdp training

--- train.sh
+++ train.sh
@@ -1,1 +1,5 @@
-python3 setup.py --seq_len 512 --batch_size 2 --grad_accumulation_steps 4 --num_epochs 1 --no-small_model --lr 0.0003 --max_grad_norm 1.0 --dataset_num_proc 32  --dataset_name roneneldan/TinyStories --subset 0.1 --local_path --torch_dtype fp32 --fsdp
+### first run the data prep
+# cd utils
+# python3 data.py python3 utils/data.py --save 1 --num_proc [num_cores] --num_partitions [num_partitions] --seq_len [seq_len]
+
+torchrun --standalone --nproc_per_node=2 setup.py --seq_len 512 --batch_size 2 --grad_accumulation_steps 8 --num_epochs 1 --no-small_model --lr 0.0003 --max_grad_norm 1.0 --local_path --torch_dtype fp32 --fsdp
